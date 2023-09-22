d3e190e22652c267bbd391bec06e14edaccc1d88
OldSchoolCoders00 <generalwandile41@gmail.com>
2023-09-22 09:19:41 +0200

    m

--- src/app/scheduled/scheduled.page.ts
+++ src/app/scheduled/scheduled.page.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
+
+export interface User {
+  id: string;
+  date: string;
+  description: string;
+  time: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-scheduled',
@@ -6,19 +16,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scheduled.page.scss'],
 })
 export class ScheduledPage implements OnInit {
-  scheduled : any[] =[];
-  today:any;
-  tomorrow:any;
+  interviewList: User[] = [];
+  isLoading = false;
+  isError = false;
 
-  constructor() { }
+  constructor(private data: DataService, private router: Router) {}
 
   ngOnInit() {
-    this.Select();
+    this.getInterview();
   }
 
-Select(){
-this.today = "Reminder for today";
-this.tomorrow = "Reminder for tomorrow";
-}
+  getInterview() {
+    this.isLoading = true;
 
+    this.data.getAllUser().subscribe(
+      (res: any[]) => {
+        this.interviewList = res.map((e: any) => {
+          const data = e.payload.doc.data();
+          data.id = e.payload.doc.id;
+          return data;
+        });
+        this.isLoading = false;
+        this.isError = false;
+      },
+      (err) => {
+        this.isLoading = false;
+        this.isError = true;
+        console.error('Error while fetching interview data', err);
+      }
+    );
+  }
+
+  onFabClick() {
+    // Navigate to the "Today" page using its route path
+    this.router.navigate(['/new-to-do']);
+  }
 }
--- src/app/today/today.page.ts
+++ src/app/today/today.page.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
-export interface User{
+export interface User {
   id: string;
   date: string;
   description: string;
-  time :string;
+  time: string;
   title: string;
 }
 
@@ -16,37 +16,43 @@ export interface User{
   styleUrls: ['./today.page.scss'],
 })
 export class TodayPage implements OnInit {
-
   interviewList: User[] = [];
-  
+  isLoading = false;
+  isError = false;
+  today: string | undefined; // Declare the 'today' variable
 
-  constructor(private data: DataService,private router: Router) { }
+  constructor(private data: DataService, private router: Router) {}
 
   ngOnInit() {
+    this.today = new Date().toISOString().slice(0, 10); // Get today's date in "YYYY-MM-DD" format
     this.getInterview();
   }
 
   getInterview() {
+    this.isLoading = true;
 
-    this.data.getAllUser().subscribe(res => {
-
-      this.interviewList = res.map((e: any) => {
-        const data = e.payload.doc.data();
-        data.id = e.payload.doc.id;
-        return data;
-      })
-
-    }, err => {
-      alert('Error while fetching interview data');
-    })
-
+    this.data.getAllUser().subscribe(
+      (res: any[]) => {
+        this.interviewList = res
+          .map((e: any) => {
+            const data = e.payload.doc.data();
+            data.id = e.payload.doc.id;
+            return data;
+          })
+          .filter((user: User) => user.date === this.today); // Filter by today's date
+        this.isLoading = false;
+        this.isError = false;
+      },
+      (err) => {
+        this.isLoading = false;
+        this.isError = true;
+        console.error('Error while fetching interview data', err);
+      }
+    );
   }
-
 
   onFabClick() {
-      // Navigate to the "Today" page using its route path
-      this.router.navigate(['/new-to-do']);
-   
+    // Navigate to the "Today" page using its route path
+    this.router.navigate(['/new-to-do']);
   }
-
 }
